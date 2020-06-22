9059641d79113c793150a4ba1ebfd6a91e3847a6
syedmateenhassan <50932203+syedmateenhassan@users.noreply.github.com>
2020-06-22 23:15:17 +0500

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.smarttracking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void signInFunc(View kuchbhi)
+    {
+
+    }
+
+    public void infoFunc(View kuchbhi)
+    {
+
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
